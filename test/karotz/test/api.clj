d25043cd634a1f1b2c2843c31075966f6d13b57f
Zilvinas Kybartas <zilvinas@inventi.lt>
2013-08-23 23:38:15 +0300

    Remove xml files from tests

--- test/karotz/test/api.clj
+++ test/karotz/test/api.clj
@@ -4,6 +4,22 @@
   (:require [clojure.xml :as xml])
   (:import java.io.IOException))
 
+(def ok-response {:tag :voosmsg, :attrs nil, :content
+                    [{:tag :id, :attrs nil, :content ["28665c15-bd73-4bbd-b2b8-02703cba8ee9"]}
+                     {:tag :correlationid, :attrs nil, :content ["73b1200d-2e23-46be-961e-a88a5590f020"]}
+                     {:tag :interactiveId, :attrs nil, :content ["INTERACTIVE-ID"]}
+                     {:tag :response, :attrs nil, :content
+                      [{:tag :code, :attrs nil, :content ["OK"]}]}]})
+
+(def error-response {:tag :voosmsg, :attrs nil, :content
+                     [{:tag :id, :attrs nil, :content ["28665c15-bd73-4bbd-b2b8-02703cba8ee9"]}
+                      {:tag :correlationid, :attrs nil, :content ["73b1200d-2e23-46be-961e-a88a5590f020"]}
+                      {:tag :interactiveId, :attrs nil, :content ["NO-INTERACTIVE-ID"]}
+                      {:tag :response, :attrs nil, :content
+                       [{:tag :code, :attrs nil, :content ["NOT_CONNECTED"]}]}]})
+
+
+
 (deftest test-error
          (testing "Should test error"
                   (is (error? "ERROR some other text"))
@@ -14,24 +30,23 @@
 
 (deftest test-karotz-request
   (with-redefs [karotz-api ""]
-    (testing "Parses correct response"
-             (is (= "INTERACTIVE-ID"
-                    (karotz-request "test-resources/ok-response.xml"))))
+      (testing "Parses correct response"
+        (with-redefs [xml/parse (constantly ok-response)]
+          (is (= "INTERACTIVE-ID"
+                 (karotz-request "url")))))
 
   (testing "Parses erroneus response"
-	  (is (= "NOT_CONNECTED"
-	         (karotz-request "test-resources/error-response.xml"))))
+    (with-redefs [xml/parse (constantly error-response)]
+      (is (= "NOT_CONNECTED"
+             (karotz-request "")))))
 
   (testing "Rethrows io exception if it occurs"
 	  (is (= IOException
            (with-redefs [xml/parse (fn [a] (throw (IOException. "problem")))]
              (try
                (karotz-request "no-such-file")
-               (catch IOException e (.getClass e)))))))
+               (catch IOException e (.getClass e)))))))))
 
-  (testing "Returns new interactive id"
-           (is (= "INTERACTIVE-ID-WITH-PARAM"
-	         (karotz-request "INTERACTIVE-ID" "test-resources/ok-response.xml"))))))
 
 (deftest test-sign-in
    (testing "Signin should return valid id"
@@ -40,5 +55,3 @@
                                  karotz-request (constantly "789")
                                  login-url (constantly "")]
                      (sign-in ["install1" "123"] {}))))))
-
-(run-tests)
